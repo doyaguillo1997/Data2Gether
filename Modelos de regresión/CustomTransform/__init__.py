79b9188b5481d91dc2fc18621f375129055db60e
ArnauFabregat <61298108+ArnauFabregat@users.noreply.github.com>
2021-09-07 23:28:50 +0200

    Delete Modelos de regresión directory

--- Modelos de regresión/CustomTransform/__init__.py
+++
@@ -1,6 +0,0 @@
-from .distanceEncoder import DistanceEncoder
-from .rangeEncoder import RangeEncoder
-from .sizeMeanEncoder import SizeMeanEncoder
-from .rangeOtherEncoder import RangeOtherEncoder
-from .knnImputer import CustomKNNImputer
-from .lowerTransformer import LowerTransformer
