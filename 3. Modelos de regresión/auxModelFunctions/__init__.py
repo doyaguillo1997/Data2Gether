d363e65213373ae5b5c46470a7fbfc89994461c4
ArnauFabregat <61298108+ArnauFabregat@users.noreply.github.com>
2021-09-08 23:07:21 +0200

    Delete 3. Modelos de regresión directory

--- 3. Modelos de regresión/auxModelFunctions/__init__.py
+++
@@ -1,7 +0,0 @@
-from .modelEvaluation import modelEvaluate
-
-from .modelPreparation import createCenteredGridSearch
-from .modelPreparation import shortCenteredGridSearch
-from .modelStorage import modelsDirectory
-
-from .modelFactory import bestModel
